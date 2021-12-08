a36f58a08f668e87e7cb4b0a8d93c782e91e3d58
Jun-Hyeok <jun.hyeok@kakao.com>
2021-12-09 04:39:28 +0900

    feat: Add `ActionType`

---
+++ project/utils/action.py
@@ -0,0 +1,25 @@
+from enum import Enum, auto
+
+
+class ActionType(Enum):
+    """ActionType is an enum class that defines the action types.
+
+    Attributes:
+        LEFT (int): Left action type.
+        RIGHT (int): Right action type.
+        UP (int): Up action type.
+        DOWN (int): Down action type.
+        NONE (int): None action type.
+    """
+
+    NONE = 0
+    FORWARD = auto()
+    LEFT = auto()
+    RIGHT = auto()
+    EAT = auto()
+
+
+if __name__ == "__main__":
+    print(ActionType.FORWARD)
+    print(ActionType.FORWARD.value)
+    print(ActionType.FORWARD.name)
